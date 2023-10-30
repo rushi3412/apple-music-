4e327d1a5f433fa0bc4c6589a1a3ca297311b468
Rushikesh <115138392+rushi3412@users.noreply.github.com>
2023-10-30 22:50:39 +0530

    Update MusicPlayerContext.js

--- src/MusicPlayerContext.js
+++ src/MusicPlayerContext.js
@@ -1,19 +1,20 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const MusicPlayerContext = createContext();
 
-export function useMusicPlayer() {
-  return useContext(MusicPlayerContext);
-}
-
 export function MusicPlayerProvider({ children }) {
-  const [currentSong, setCurrentSong] = useState(null);
-
-  // You can add additional state and functions here for playback control.
+  const [currentSong, setCurrentSong] = useState([]);
+  const [indexSong, setIndexSong] = useState(0);
+  const [songs, setSongs] = useState([]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
-    <MusicPlayerContext.Provider value={{ currentSong, setCurrentSong }}>
+    <MusicPlayerContext.Provider value={{ currentSong, setCurrentSong, songs, indexSong, setIndexSong, isPlaying, setIsPlaying }}>
       {children}
     </MusicPlayerContext.Provider>
   );
+}
+
+export function useMusicPlayer() {
+  return useContext(MusicPlayerContext);
 }
