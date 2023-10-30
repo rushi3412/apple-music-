d6f81af4bb8dc2eedf4c2b48325368f7ff936b9c
Rushikesh <115138392+rushi3412@users.noreply.github.com>
2023-10-30 22:50:09 +0530

    Update Maindiv.js

--- src/Maindiv.js
+++ src/Maindiv.js
@@ -1,17 +1,3 @@
-// import React from "react";
-
-// const MainContent = ({ children }) => {
-//   return (
-//     <div style={{ marginLeft: "70px", padding: "10px", marginTop: "90px"}}>{children}</div>
-//   );
-// };
-
-// export default MainContent;
-
-
-
-
-
 import React from "react";
 function Maindiv() {
   return <h6>Rushikesh</h6>;
