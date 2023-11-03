509f95d0ecf91b19f413cb147b43bebcdb8bfeab
Rushikesh <115138392+rushi3412@users.noreply.github.com>
2023-11-03 13:42:28 +0530

    Update SignIn.js

--- src/SignIn.js
+++ src/SignIn.js
@@ -83,6 +83,7 @@ const SignIn = () => {
       alignItems: 'center',
       justifyContent: 'center',
       width: "700px",
+      boxShadow: '0px 0px 10px white',
       height: "600px",
       margin: "0 auto",
       fontFamily: "Arial",
@@ -92,11 +93,29 @@ const SignIn = () => {
       background: '#1d1c1c',
       top: '40%',
       left: '50%',
+      borderRadius: '10px',
       transform: 'translate(-50%, -50%)',
       overflow: 'auto',
     }}>
       {showEmailForm ? (
         <>
+          <div className="apple-logo">
+                      <svg
+                        viewBox="0 0 18 18"
+                        x="0px"
+                        y="0px"
+                        width="50px"
+                        height="50px"
+                        data-test="apple-logo"
+                      >
+                        <path
+                          d="M8.8,5.2c-0.7,0-1.8-0.8-3-0.8c-1.5,0-2.9,0.9-3.7,2.3c-1.6,2.8-0.4,6.8,1.1,9.1C4,16.8,4.9,18,6.1,18c1.1,0,1.6-0.7,3-0.7
+            c1.4,0,1.8,0.7,3,0.7c1.2,0,2-1.1,2.8-2.2c0.9-1.3,1.2-2.5,1.2-2.6c0,0-2.4-0.9-2.4-3.6c0-2.3,1.9-3.4,1.9-3.4
+            c-1.1-1.6-2.7-1.7-3.3-1.8C10.7,4.2,9.5,5.2,8.8,5.2z M11.3,2.9c0.6-0.8,1.1-1.8,0.9-2.9c-0.9,0-2,0.6-2.6,1.4C9,2,8.5,3.1,8.6,4.1
+            C9.6,4.2,10.7,3.6,11.3,2.9"
+                        ></path>  
+                      </svg>
+                      </div>
           <h1 style={{ marginBottom: '20px' }}>Sign In or Sign Up</h1>
           <h2 style={{ marginBottom: '20px' }}>Enter your email to get started.</h2>
           <label style={{ marginBottom: '20px' }}>
