436fd8fe914bd3192356b6fe966adf56b7412f9e
Rushikesh <115138392+rushi3412@users.noreply.github.com>
2023-11-08 12:54:31 +0530

    Update MusicPlayerContext.js

--- src/MusicPlayerContext.js
+++ src/MusicPlayerContext.js
@@ -1,15 +1,44 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const MusicPlayerContext = createContext();
 
 export function MusicPlayerProvider({ children }) {
-  const [currentSong, setCurrentSong] = useState([]);
+  const [currentSong, setCurrentSong] = useState(null);
+  const [playlist, setPlaylist] = useState([]);
   const [indexSong, setIndexSong] = useState(0);
-  const [songs, setSongs] = useState([]);
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const playSong = (song, songList) => {
+    console.log('Playing song:', song);
+    console.log('Playlist:', songList);
+    setCurrentSong(song);
+    setPlaylist(songList);
+    setIndexSong(songList.findIndex((item) => item._id === song._id));
+    setIsPlaying(true);
+  };
+
+  const playNextSong = () => {
+    const nextIndex = (indexSong + 1) % playlist.length;
+    console.log('Playing next song:', playlist[nextIndex]);
+    setCurrentSong(playlist[nextIndex]);
+    setIndexSong(nextIndex);
+  };
+
+  const playPreviousSong = () => {
+    const prevIndex = (indexSong - 1 + playlist.length) % playlist.length;
+    console.log('Playing previous song:', playlist[prevIndex]);
+    setCurrentSong(playlist[prevIndex]);
+    setIndexSong(prevIndex);
+  };
+
+  const togglePlayPause = () => {
+    console.log('Before toggle, isPlaying:', isPlaying);
+    setIsPlaying(!isPlaying);
+    console.log('After toggle, isPlaying:', !isPlaying);
+  };
+
   return (
-    <MusicPlayerContext.Provider value={{ currentSong, setCurrentSong, songs, indexSong, setIndexSong, isPlaying, setIsPlaying }}>
+    <MusicPlayerContext.Provider value={{ currentSong, setCurrentSong, playlist, indexSong, setIndexSong, isPlaying, playSong, togglePlayPause, playNextSong, playPreviousSong }}>
       {children}
     </MusicPlayerContext.Provider>
   );
